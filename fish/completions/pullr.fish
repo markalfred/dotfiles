b71fc63a569ef3726e74146cb756a279dc25652c
Mark Battersby <mark@mojotech.com>
2014-05-19 11:41:57 -0400

    Update pullr completion.
    Syntax for remote branch is special. Remote name must be removed. This
    only supports 'origin' as a remote currently.

--- fish/completions/pullr.fish
+++ fish/completions/pullr.fish
@@ -1,8 +1,13 @@
 # Since fish loads completion files only when needed, this function
 # (from completions/git.fish) could be missing.
-function __fish_git_branches
-  command git branch --no-color -a ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///'
+function __fish_local_git_branches
+  command git branch --no-color ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//'
 end
 
-complete --command pullr --short f --long from --no-files --arguments '(__fish_git_branches)'
-complete --command pullr --short i --long into --no-files --arguments '(__fish_git_branches)'
+function __fish_remote_git_branches
+  # Pullr takes the "local" style name. Never show "origin".
+  command git branch -r --no-color ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//' -e 's/^origin\///'
+end
+
+complete --exclusive --command pullr --short f --long from --no-files --arguments '(__fish_local_git_branches)'
+complete --exclusive --command pullr --short i --long into --no-files --arguments '(__fish_remote_git_branches)'
