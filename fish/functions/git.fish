d9c275cdca95d8937d96920862e0fc573e05a49c
Mark Battersby <mark@mojotech.com>
2014-06-20 11:16:57 -0400

    Prevent git push from pushing master.

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,17 +1,26 @@
 function git
-  if [ (count $argv) -gt 0 ]
-    command git $argv
-  else if [ $interactive_git_mode ]
-    # Already in interactive git mode. Do nothing.
-  else
-    set -g interactive_git_mode true
-
-    # Allow graceful exit back to default prompt.
-    function exit
-      set -e interactive_git_mode
-      commandline -r ''
-      commandline -f repaint
-      functions -e exit
+  if [ (count $argv) = 0 ]
+    if [ $interactive_git_mode ]
+      # Already in interactive git mode. Do nothing.
+    else
+      set -g interactive_git_mode true
+      # Allow graceful exit back to default prompt.
+      function exit
+        set -e interactive_git_mode
+        commandline -r ''
+        commandline -f repaint
+        functions -e exit
+      end
     end
+
+  else if [ push = $argv[1] ]
+    if [ (_git_branch_name) = 'master' ]
+      echo "Dude, you're on master."
+    else
+      command git $argv
+    end
+
+  else
+    command git $argv
   end
 end
