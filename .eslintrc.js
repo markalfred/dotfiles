08f6c1f04e7e3a1bb1335e0dba3ad87d8721e9ed
Mark Battersby <mark@mojotech.com>
2016-04-06 18:10:20 -0400

    Add base eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: 'babel-eslint',
+  ecmaFeatures: {},
+  extends: 'eslint:recommended',
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+    node: true
+  },
+  rules: {
+    'block-spacing': [2, 'always'],
+    'comma-dangle': 2,
+    'dot-location': [2, 'property'],
+    'dot-notation': 2,
+    'eqeqeq': 2,
+    'indent': [2, 2],
+    'object-curly-spacing': [2, 'always'],
+    'no-console': 1,
+    'no-debugger': 1,
+    'no-multi-spaces': 2,
+    'no-multiple-empty-lines': [2, { max: 1 }],
+    'no-shadow': 2,
+    'no-undef': 2,
+    'no-unused-vars': 2,
+    'quotes': [2, 'single', 'avoid-escape']
+  },
+  plugins: [],
+  globals: {
+    chai: true
+  }
+};
