84a839726920079664d970c9814f70425bfbcdc4
Mark Battersby <mark.alfred.battersby@gmail.com>
2022-02-09 14:05:10 -0500

    Drop rbenv init

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,6 @@ eval (/opt/homebrew/bin/brew shellenv)
 # Initialize asdf.
 source (brew --prefix asdf)/asdf.fish
 
-# Initialize rbenv.
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null
-
 # Add python bins.
 set PATH $PATH /usr/local/Python/bin
 
